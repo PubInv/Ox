360a6b0bb0b15e734b614e898aac4f1f7fa168d0
pranavs1911 <spranav1911@gmail.com>
2021-07-10 17:33:08 -0400

    "Version 3.0 : Made some minor changes"
    Reviewed your comments and made the changes you suggested.

--- firmware/lib/PIDC/PIDCnew.cpp
+++ firmware/lib/PIDC/PIDCnew.cpp
@@ -3,9 +3,16 @@
  *
  *    Description:  
  *
+<<<<<<< HEAD
  *        Version:  1.0
  *        Created:  06/11/2021
  *       Revision:  none
+=======
+ *        Version:  3.0
+ *        Created:  07/09/2021
+ *       Revision:  Revised after Version 2.0
+ *       
+>>>>>>> develop
  *         Author:  Pranav Shankar Srinivasan (spranav1911@gmail.com)
  * =====================================================================================*/
 #include <stdio.h>
@@ -39,11 +46,19 @@ namespace PIDController
     bool isOn = vt->isOn;
     int msL = vt->msLast;
     int pin = vt->pin;
+<<<<<<< HEAD
     int16_t sum = 0;
     int integral = 0;
     int deriv = 0;
     int error[] = {3, 4, 5};     //Initialized as dupe array
     int pressure[] = {1.4, 5.6}; //Initialized as dupe array
+=======
+    float sum = 0;
+    float integral = 0;
+    float deriv = 0;
+    float error[] = {3, 4, 5};     //Initialized as dupe array
+    float pressure[] = {1.4, 5.6}; //Initialized as dupe array
+>>>>>>> develop
     PIOC_Controller::Valve *valve = valveArray;
     ControlGains c;
 
@@ -53,6 +68,7 @@ namespace PIDController
             printf("PID Controller can be initialized");
         return;
     }
+<<<<<<< HEAD
 
     void startingGains(int kp, int kd, int ki, ValveState vt)
     {
@@ -61,6 +77,21 @@ namespace PIDController
             c.kp = 0.5;
             c.kd = 0.5;
             c.ki = 0.5;
+=======
+    void initGains(float a, float b, float y)
+    {
+        c.kp = a;
+        c.ki = b;
+        c.kd = y;
+        return;
+    }
+
+    void startingGains(float kp, float ki, float kd, ValveState vt)
+    {
+        if (vt.status == 0)
+        {
+            initGains(0.5, 0.5, 0.5);
+>>>>>>> develop
         }
         return;
     }
@@ -68,6 +99,7 @@ namespace PIDController
     {
         if (stat == 0 && (pres >= (1.17 * minpres)) && mod == RUNNING) //If the desired pressure is too high and the current pressure is well below the desired pressure.
         {
+<<<<<<< HEAD
             c.kp = 0.65; // kp is increased to reach the desired pressure quickly. ki and kd are still low.
             c.ki = 0.56;
             c.kd = 0.34;
@@ -77,14 +109,27 @@ namespace PIDController
             c.kp = 0.54; // kp is reduced since we are close to reaching the desired pressure. ki and kd are increased since the pressure is already near the desired pressure at the starting phase.
             c.kd = 0.57;
             c.ki = 0.63;
+=======
+            initGains(0.65, 0.56, 0.34); // kp is increased to reach the desired pressure quickly. ki and kd are still low.
+        }
+        else if (stat == 0 && (pres <= 0.85 * maxpres) && mod == RUNNING) // If the desired pressure is high and the current pressure is close to the desired pressure.
+        {
+            initGains(0.54, 0.63, 0.57); // kp is reduced since we are close to reaching the desired pressure. ki and kd are increased since the pressure is already near the desired pressure at the starting phase.
+>>>>>>> develop
         }
         //If the desired pressure is too low and the current pressure limits is well above the desired pressure.
         else if (stat == 0 && (pres <= (1.27 * minpres)) && mod == RUNNING)
         {
+<<<<<<< HEAD
             c.kp = 0.59; //kp is slightly increased since we need to desired pressure quickly. ki and kd are slightly increased too to reduce oscillations when we are nearing the desired pressure.
             c.kd = 0.623;
             c.ki = 0.367;
         }
+=======
+            initGains(0.59, 0.367, 0.623); //kp is slightly increased since we need to desired pressure quickly. ki and kd are slightly increased too to reduce oscillations when we are nearing the desired pressure.
+        }
+        return;
+>>>>>>> develop
     }
     void InitialControlGainsValve(ValveStatus *vs, ValveState *vt, PIOCState *pstate, SensorState *st)
     {
@@ -92,6 +137,7 @@ namespace PIDController
         { //Checks for error at the start.
             if ((onT < 0.4 * offT))
             {
+<<<<<<< HEAD
                 c.kp = 0.56;
                 c.kd = 0.43; // Derivative Gain is double the instance when the onTime is increased.
                 c.ki = 0.45; //Integral gain is slightly raised to compensate for the state error.
@@ -107,13 +153,35 @@ namespace PIDController
     //Need to check about tackling MISSED and ERROR
     //Need to shiftOutValves to alter the timing of the oxygen concentrator.
     void multiplyGains(int x, int y, int z)
+=======
+                initGains(0.56, 0.45, 0.43);
+                // Derivative Gain is double the instance when the onTime is increased.
+                //Integral gain is slightly raised to compensate for the state error.
+            }
+            else
+            {
+                initGains(0.55, 0.65, 0.63);
+                // Derivative Gain is double the instance when the onTime is increased.
+                //Integral gain is slightly raised to compensate for the state error.
+            }
+        }
+        return;
+    }
+    //Need to check about tackling MISSED and ERROR
+    //Need to shiftOutValves to alter the timing of the oxygen concentrator.
+    void multiplyGains(float x, float y, float z)
+>>>>>>> develop
     {
         c.kp = c.kp * x;
         c.ki = c.ki * y;
         c.kd = c.kd * z;
         return;
     }
+<<<<<<< HEAD
     int computeSum(int i, int error[])
+=======
+    float computeSum(int i, float error[])
+>>>>>>> develop
     {
         uint16_t t = 1; // Checks for every 1 millisecond.
         if (i > 0)
@@ -134,8 +202,23 @@ namespace PIDController
     void changeTiming(int i, float a){
         valve[i].start = a*valve[i].start + valve[i].start;
         valve[i+2].start = a*valve[i].stop + valve[i].stop; 
+<<<<<<< HEAD
     }
     int16_t ControllerComp(SensorState *st, ValveState *vt)
+=======
+        return;
+    }
+    void immediateChange(int j, PIOC_Controller::Valve *valve){
+        valve[j].stop = valve[j].stop + 700; //Making sure the stop time of the current valve is increased and the start time of the valve at the outlet is decreased to increase oxygen flow.
+        valve[j + 2].start = valve[j + 2].start - 700;
+        if (((error[j] - error[j - 1]) > 0.06)){
+            valve[j].state = false; // Closing the current valve and opening the next valve to make sure that the O2 goes through the sink and not through the outlet.
+            valve[j].stop = valve[j].stop - 700;
+        }
+        return;
+    }
+    float ControllerComp(SensorState *st, ValveState *vt)
+>>>>>>> develop
     {
         uint64_t ms = PIOC_Timer::timeSinceEpochMs();
         for (int j = 0; j++; j <= 3)
@@ -206,22 +289,34 @@ namespace PIDController
                         { //We are checking the last one second and we are checking if there is an error. We have to check if the pressure at the pressure sensor is above/below the desired pressure. We have to close/open the subsequent valves accordingly.
                             if (j == 0)
                             {
+<<<<<<< HEAD
                                 valve[j].state = false; // Closing the current valve and opening the next valve to make sure that the O2 goes through the sink and not through the outlet.
                                 valve[j].stop = valve[j].stop - 700;
+=======
+                                immediateChange(j, valve);
+>>>>>>> develop
                                 valve[j + 1].state = true;
                                 valve[j + 1].start = valve[j + 1].start - 700;
                                 multiplyGains(1.17, 1.19, 1.0);
                             }
                             else if (j == 1)
                             {
+<<<<<<< HEAD
                                 valve[j].state = false; // Closing the current valve and opening the next valve to make sure that the O2 goes through the sink and not through the outlet.
                                 valve[j].stop = valve[j].stop - 700;
+=======
+                                immediateChange(j, valve);
+>>>>>>> develop
                                 valve[j - 1].state = true;
                                 valve[j - 1].stop = valve[j - 1].stop + 700;
                                 multiplyGains(1.17, 1.19, 1.0);
                             }
                         }
+<<<<<<< HEAD
                         else if (((error[i] - error[i - 1]) < -0.06) && (offT - i) <= 2000)
+=======
+                        else if (((error[i] - error[i - 1]) < -0.06) && (offT - i) <= 1500)
+>>>>>>> develop
                         { //We are checking the last one second and we are checking if there is an error. We have to check if the pressure at the pressure sensor is above/below the desired pressure. We have to close/open the subsequent valves accordingly.
                             //Change valve Timing
                             if (j == 0)
@@ -229,8 +324,12 @@ namespace PIDController
                                 if (valve[j].state == 1)
                                 {
                                     valve[j + 1].state = 0;              //Close the immediate valve to make sure O2 does not go to sink.
+<<<<<<< HEAD
                                     valve[j].stop = valve[j].stop + 700; //Making sure the stop time of the current valve is increased and the start time of the valve at the outlet is decreased to increase oxygen flow.
                                     valve[j + 2].start = valve[j + 2].start - 700;
+=======
+                                    immediateChange(j, valve);
+>>>>>>> develop
                                     multiplyGains(1.17, 0.85, 1.12);
                                 }
                             }
@@ -239,15 +338,23 @@ namespace PIDController
                                 if (valve[j].state == 1)
                                 {
                                     valve[j - 1].state = 0;              //Close the immediate valve to make sure O2 does not go to sink.
+<<<<<<< HEAD
                                     valve[j].stop = valve[j].stop + 700; //Making sure the stop time of the current valve is increased and the start time of the valve at the outlet is decreased to increase oxygen flow.
                                     valve[j + 2].start = valve[j + 2].start - 700;
+=======
+                                    immediateChange(j, valve);
+>>>>>>> develop
                                     multiplyGains(1.17, 0.85, 1.12);
                                 }
                             }
                         }
                         if (error[i] >= 1.3 * error[i - 1])
                         {
+<<<<<<< HEAD
                             multiplyGains(1.15, 1.19, 1.2); // TODO: Check how the alternate set of valve states can be changed for each change in error state. 
+=======
+                            multiplyGains(1.15, 1.19, 1.2); // TODO: Check how the alternate set of valve states can be changed for each change in error state.
+>>>>>>> develop
                         }
                         sum = computeSum(i, error);
                     }
@@ -265,6 +372,7 @@ namespace PIDController
     void ImplementController(SensorState *st, ValveState *vt, ValveStatus *vs, PIOCState *pstate)
     {
         uint16_t ms = PIOC_Timer::timeSinceEpochMs();
+<<<<<<< HEAD
         PIDControl p;
         p.InitialControlGainsSensor(st, pstate);
         p.InitialControlGainsValve(vs, vt, pstate, st);
@@ -281,5 +389,22 @@ namespace PIDController
                     changeTiming(i,con);
                 }
         }
+=======
+        InitialControlGainsSensor(st, pstate);
+        InitialControlGainsValve(vs, vt, pstate, st);
+        float con = ControllerComp(st, vt);
+        for (int i = 0; i++; i <= ms)
+        {
+            for (int j = 0; j++; j <= 3)
+            {
+                if (valve[j].start <= ms <= valve[j].stop)
+                    if (valve[j].state == 1 && (i == 0 || i == 1))
+                    {
+                        changeTiming(i, con);
+                    }
+            }
+        }
+        return;
+>>>>>>> develop
     };
 }
--- firmware/lib/PIDC/PIDCnew.h
+++ firmware/lib/PIDC/PIDCnew.h
@@ -3,9 +3,15 @@
  *
  *    Description:  
  *
+<<<<<<< HEAD
  *        Version:  1.0
  *        Created:  06/11/2021
  *       Revision:  none
+=======
+ *        Version:  3.0
+ *        Created:  07/09/2021
+ *       Revision:  Revised after Version 2.0
+>>>>>>> develop
  *       
  *         Author:  Pranav Shankar Srinivasan (spranav1911@gmail.com)
  * =====================================================================================*/
@@ -29,15 +35,22 @@ namespace PIDController
     //Referencing update valve timing from Valve
     struct ControlGains
     {
+<<<<<<< HEAD
         uint16_t kp;
         uint16_t kd;
         uint16_t ki;
+=======
+        float kp;
+        float kd;
+        float ki;
+>>>>>>> develop
     };
 
     //Referencing MPRPressure.cpp for getting pressure sensor readings
 
     class PIDControl
     {
+<<<<<<< HEAD
     public:
         ControlGains c;
         void checkifSystemisOn(SensorState *st, PIOCState *pstate);
@@ -48,6 +61,18 @@ namespace PIDController
         int computeSum(int i, int error[]);
         void changeTiming(int i, float a);
         int16_t ControllerComp(SensorState *st, ValveState *vt);
+=======
+        ControlGains c;
+        void checkifSystemisOn(SensorState *st, PIOCState *pstate);
+        void startingGains(float kp, float ki, float kd, ValveState vt);
+        void InitialControlGainsSensor(SensorState *st, PIOCState *pstate);
+        void InitialControlGainsValve(ValveStatus *vs, ValveState *vt, PIOCState *pstate, SensorState *st);
+        void multiplyGains(float x, float y, float z);
+        float computeSum(float i, float error[]);
+        void changeTiming(int i, float a);
+        void immediateChange(int j,PIOC_Controller::Valve *valve);
+        float ControllerComp(SensorState *st, ValveState *vt);
+>>>>>>> develop
         void ImplementController(SensorState *st, ValveState *vt, ValveStatus *vs, PIOCState *pstate);
     };
 }
