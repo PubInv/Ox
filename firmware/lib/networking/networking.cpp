c7569985de9d8431c564ad0673abadf7f050ca54
Ben Coombs <bencoombs@protonmail.com>
2021-09-30 20:00:16 +1300

    Add networking library

--- firmware/lib/networking/networking.cpp
+++ firmware/lib/networking/networking.cpp
@@ -1,2 +1,23 @@
+#include <debug.h>
 #include <networking.h>
+namespace VentOS {
 
+bool NetworkingController::setup(VentController *vc) {
+  DebugLnCC("Networking setup\n");
+  VentOS_Networking::server_init(vc);
+  return true;
+}
+
+bool NetworkingController::run(uint32_t msNow) {
+  VentOS_Networking::server_poll();
+  return true;
+}
+
+bool NetworkingController::connect() { return false; }
+
+bool NetworkingController::send_udp(const void *data, size_t s) {
+  VentOS_Networking::server_send_udp(data, s);
+  return true;
+}
+
+} // namespace VentOS
--- firmware/lib/networking/networking.h
+++ firmware/lib/networking/networking.h
@@ -1,5 +1,24 @@
 #ifndef NETWORKING_H
 #define NETWORKING_H
 
+#include <PIRCS.h>
+#include <PIRDS.h>
+#include <controller.h>
+#include <server.h>
+#include <task.h>
+
+// using Ox_Networking;
+
+namespace Ox {
+
+class NetworkingController : public Task {
+public:
+  bool setup(VentController *vc);
+  bool run(uint32_t msNow);
+  bool connect();
+  bool send_udp(const void *data, size_t s);
+};
+
+} // namespace Ox
 
 #endif
