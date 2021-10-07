2f9087b4aef82ded392f79904511678ef6c786fa
Ben Coombs <bencoombs@protonmail.com>
2021-10-08 01:10:22 +1300

    Websocket emit data to page

--- web/server.js
+++ web/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { createServer } = require("http");
-const Server = require("socket.io");
-
 const app = express();
-var cookieSession = require("cookie-session");
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = 3005;
+
+/*var cookieSession = require("cookie-session");
 app.set('trust proxy', 1)
 app.use(
     cookieSession({
@@ -14,10 +15,7 @@ app.use(
         httpOnly: true,
         sameSite: 'none'
     })
-);
-const httpServer = createServer(app);
-const io = new Server(httpServer, { serveClient: false });
-const port = 3005;
+);*/
 
 
 
@@ -28,7 +26,28 @@ io.on('connection', (socket) => {
     console.log(`a user connected ${socket.id}`);
 });
 
+
+var cnt = 0;
+let freq = 0.1; // Hz
+let amp = 2; // amplitude
+
+let d_0 = new Date();
+let t_0 = d_0.getTime();
+
+setInterval(function () {
+
+    var time = new Date();
+    let t_val = (time.getTime() - t_0) / 1000;
+    let w = 2 * Math.PI * freq;
+    var y_val = amp * Math.sin(w * (t_val));
+
+    point = { t: t_val, y: y_val };
+    io.emit('plot', point);
+
+    if (++cnt === 10000) clearInterval(interval);
+}, 100);
+
 // Start server
-httpServer.listen(port, () => {
+server.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
