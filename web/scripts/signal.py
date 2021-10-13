e436367baebf68c8c23ae1d5e807fbb8165a7cb5
Ben Coombs <bencoombs@protonmail.com>
2021-10-14 00:00:53 +1300

    Fix typos, add comments.

--- web/scripts/signal.py
+++ web/scripts/signal.py
@@ -4,14 +4,17 @@ import json
 import time
 import math
 
+##################################################
+# Simple mock signal generator script to post    #
+# data to the api endpoint                       #
+##################################################
+
 URL = "http://localhost:3020/api/pimd"
 
 # Equivalent ISO time formate to JavaScript
 def iso_format_js(dt):
     isostring = datetime.strftime(dt, '%Y-%m-%dT%H:%M:%S.{0}Z')
     return isostring.format(int(round(dt.microsecond/1000.0)))
-
-print
 
 i = 0
 while i < 1000:
