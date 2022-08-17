9ce51322e1d48b38e11792a125b053f8beebdcfd
Robert L. Read <read.robert@gmail.com>
2022-08-17 17:48:16 -0500

    adding this

---
+++ firmware/lib/hal/SensirionSFM3X00.h
@@ -0,0 +1,62 @@
+// Copyright (C) 2021
+// Robert Read.
+
+// This program includes free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+
+// See the GNU Affero General Public License for more details.
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+
+// This is for a MostPlus M3037 Mass Air Flow sensor.
+// We don't have good specs on this. It is a simple 12V
+// hot wire sensor, that gives a voltage out. The voltage varies
+// goes up as the flow increases.
+// At the moment we are using this only as a safety features
+// to make sure that air is flowing; we have no way to calibrate it.
+// Furtheremore, to read the value with a Due we need a voltage divider
+// circuit. I created a divider with a 22Kohm and a 47kohm resistor,
+// which puts the zero-flow voltage at about 1.8 volts. Sadly, our
+// fan only raises this a few tenths of a volt.
+// We probably should get a different hot-wire sensor, and
+// also the physical housing of this sensor is much larger than
+// our main air flow, which creates a problem in several ways.
+// But, this is a proof-of-concept. Robert L. Read
+
+#ifndef SENSIRIONSFM3X00_FLOW
+#define SENSIRIONSFM3X00_FLOW
+
+#ifdef ARDUINO
+#include <Arduino.h>
+#include <SFM3X00.h>
+
+#else
+#include <cstdint>
+#endif
+
+#include <model.h>
+
+class SensirionFlow {
+public:
+  SensirionFlow();
+  const float MINIMUM_FLOW_ml_per_s = 20; // Completely arbitrary
+
+  SFM3X00 *flowSensor;
+
+  float flowInSLM();
+  float flowIn_ml_per_s();
+
+  boolean isAirFlowing();
+
+  virtual ~SensirionFlow() {};
+};
+
+
+#endif
