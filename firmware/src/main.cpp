25ef9d9baa3cfbba8ad70579ac9710b2a16e1c9e
Robert L. Read <read.robert@gmail.com>
2023-10-09 09:48:10 -0500

    reving version for PathfinderCandidate

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -18,7 +18,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #define COMPANY_NAME "pubinv.org "
 //#define PROG_NAME "main.cpp"
 #define PROG_NAME "OEDCS"
-#define VERSION "; Rev: 0.3.6"  // Pathfinder Relase Candidate (PID changes)
+#define VERSION "; Rev: 0.3.7"  // Pathfinder Relase Candidate (PID changes)
 #define DEVICE_UNDER_TEST "Hardware: Due"  //A model number
 #define LICENSE "GNU Affero General Public License, version 3 "
 
