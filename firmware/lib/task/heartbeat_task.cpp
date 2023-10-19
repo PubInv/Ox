b86d32904f6adfec9c0d5d30cd3b4900a3ba5f99
Forrest Erickson <forresterickson@comcast.net>
2023-10-19 14:11:47 -0400

    Add initial files for heartbeat task.

---
+++ firmware/lib/task/heartbeat_task.cpp
@@ -0,0 +1,38 @@
+/*
+This program includes free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+See the GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+#include "heartbeat_task.h"
+#include <cmath>
+#include <abstract_temperature.h>
+
+using namespace std;
+
+
+namespace OxApp
+{
+    bool HeartbeatTask::_init()
+    {
+        OxCore::Debug<const char *>("HeartbeatTask init\n");
+
+        return true;
+    }
+
+    bool HeartbeatTask::_run()
+    {
+      // Note:adding a heartbeat task
+        return true;
+    }
+
+}
---
+++ firmware/lib/task/heartbeat_task.h
@@ -0,0 +1,55 @@
+/*
+ Copyright (C) 2022 Robert L. Read
+
+ This program includes free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+See the GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+#ifndef HEARTBEAT_TASK_H
+#define HEARTBEAT_TASK_H
+
+
+#ifdef ARDUINO
+#include <Arduino.h>
+#endif
+#include <core.h>
+#include "../collections/array.h"
+#include "heater.h"
+#include <machine.h>
+
+#include <abstract_temperature.h>
+
+#ifndef RIBBONFISH
+#include <mock_temperature_sensor.h>
+#else
+#include <DS18B20_temperature.h>
+#endif
+
+
+#include "model.h"
+
+
+namespace OxApp
+{
+    // Runs the Pressure Swing Adsorption cycle
+    class HeartbeatTask : public OxCore::Task
+    {
+    public:
+    private:
+      bool _init() override;
+      bool _run() override;
+      // This would go into the abstract class.
+    };
+}
+
+#endif
